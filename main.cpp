2adc36fa9281b9e4f672ce812fae72f88e7e2d12
toeger <toeger@anonymouse.me>
2016-05-26 02:11:13 +0200

    added counters, updates and exception handling

--- main.cpp
+++ main.cpp
@@ -12,9 +12,6 @@
 #include <map>
 #include <memory>
 
-static const auto libdirs = {"/lib", "/usr/lib"};
-static const auto data_base_file = "~/.libfinder/database"; //TODO: find a way to share the database between users
-
 std::string get_output_from_command(std::experimental::string_view command) {
 	auto fp = popen(command.data(), "r");
 	if (!fp) {
@@ -37,8 +34,21 @@ std::string get_output_from_command(std::experimental::string_view command) {
 	return buffer;
 }
 
+static const auto libdirs = {"/lib", "/usr/lib"};
+static const auto data_base_path = [] {
+	auto username = get_output_from_command("whoami");
+	username.pop_back(); //remove newline
+	return "/home/" + username + "/.libfinder";
+}();
+static const auto data_base_file = data_base_path + "/database"; //TODO: find a way to share the database between users
+
 void add_to_database(std::map<std::string, std::string> &symbol_file_map, const std::experimental::string_view dir) {
+	static int symbols = 0;
+	static int libs = 0;
+	static int files = 0;
 	for (auto &file : boost::filesystem::recursive_directory_iterator(boost::filesystem::path(dir.data()))) {
+		files++;
+		std::cout << "\rfound " << symbols << " symbols in " << libs << " libs of " << files << " files" << std::flush;
 		auto file_command_output = get_output_from_command("file -L " + file.path().string());
 		if (file_command_output.find("symbolic link to ") != std::string::npos) {
 			continue;
@@ -49,6 +59,7 @@ void add_to_database(std::map<std::string, std::string> &symbol_file_map, const 
 		if (file_command_output.find("ELF 64-bit LSB shared object") == std::string::npos) {
 			continue;
 		}
+		libs++;
 		std::stringstream ss(get_output_from_command("objdump -TCw  " + file.path().string()));
 		std::string line;
 		std::getline(ss, line); //empty line
@@ -70,9 +81,9 @@ void add_to_database(std::map<std::string, std::string> &symbol_file_map, const 
 			}
 			auto symbol_pos = line.data() + rinit - 1;
 			while (*symbol_pos++ != ' ') {
-				;
 			}
 			symbol_file_map[symbol_pos] += ':' + file.path().string();
+			symbols++;
 		}
 	}
 }
@@ -156,7 +167,7 @@ const char *find_files(std::experimental::string_view data, std::experimental::s
 
 std::vector<std::string> lookup(std::experimental::string_view symbol) {
 	std::vector<std::string> retval;
-	boost::interprocess::file_mapping file(data_base_file, boost::interprocess::read_only);
+	boost::interprocess::file_mapping file(data_base_file.c_str(), boost::interprocess::read_only);
 	boost::interprocess::mapped_region region(file, boost::interprocess::read_only);
 	std::experimental::string_view data(static_cast<const char *>(region.get_address()), region.get_size());
 	auto files = find_files(data, symbol);
@@ -175,21 +186,36 @@ std::vector<std::string> lookup(std::experimental::string_view symbol) {
 
 int main(int argc, char *argv[]) {
 	boost::program_options::options_description options("Parameters");
+	int jobs;
 	options.add_options()("help,h", "print this")("update,u", "update lookup table")("symbol,s", boost::program_options::value<std::string>(),
-																					 "the symbol to look up");
+																					 "the symbol to look up")(
+		"jobs,j", boost::program_options::value<int>(&jobs)->default_value(1), "specify the maximum number of threads to use");
 	boost::program_options::variables_map variables_map;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), variables_map);
+	try {
+		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), variables_map);
+	} catch (const boost::program_options::error &e) {
+		std::cout << "error: " << e.what() << '\n' << "run '" << argv[0] << " --help' for details\n";
+		return -1;
+	}
 	boost::program_options::notify(variables_map);
-	if (variables_map.count("update")) {
-		std::ofstream db_file(data_base_file);
-		create_database(db_file, libdirs);
-	} if (variables_map.count("help")) {
+	if (variables_map.count("help")) {
 		std::cout << options;
-	} if (variables_map.count("symbol")) {
+	}
+	if (variables_map.count("update")) {
+		boost::filesystem::create_directory(data_base_path);
+		std::ofstream db_file(data_base_file, std::ios_base::out);
+		if (!db_file) {
+			std::cerr << "failed opening database file " << data_base_file << '\n';
+			return -1;
+		}
+		create_database(db_file, libdirs);
+	}
+	if (variables_map.count("symbol")) {
 		const auto &files = lookup(variables_map["symbol"].as<std::string>());
 		for (auto &file : files) {
-			std::cout << file << '\n';
+			std::cout << file;
 			break;
 		}
 	}
+	std::cout << '\n';
 }
