c67142aa1238ac09f9fa810ccc5a323ed51853a8
toeger <toeger@anonymouse.me>
2016-08-28 18:50:07 +0200

    added the goal

--- radix_tree.h
+++ radix_tree.h
@@ -1,6 +1,15 @@
 #ifndef RADIX_TREE_H
 #define RADIX_TREE_H
 
+/*
+ * This radix tree shall implement a std::map<std::string, std::string> in a more efficient way.
+ * It is more memory efficient by saving common prefixes only once in both the key and the value.
+ * Nodes in the tree need to have stable iterators and it must be possible to reconstruct the key from a Node.
+ * The tree needs to be serialized to disk.
+ * Lookups need to be efficient on a serialized tree.
+ * The memory representation does not need to be efficient.
+ */
+
 #include <experimental/string_view>
 #include <string>
 #include <vector>
