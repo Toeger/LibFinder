518dd5cda5e742b5087a4899abc154e81b5ece34
toeger <toeger@anony.mous>
2026-01-25 12:33:42 +0100

    Missed some files

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #include "argument_parser.h"
 #include "asserts.h"
 #include "libparser.h"
-#include "test_radix_tree.h"
+#include "reverse_prefix_tree.h"
 
 static void test_reading_arguments() {
 	char short_help[] = "-h";
@@ -20,18 +20,39 @@ static void test_reading_arguments() {
 }
 
 static void test_symbol_loading() {
-	const auto &symbols = parse_lib("/usr/lib/x86_64-linux-gnu/libboost_program_options.so", true);
-	const auto symbol_to_find = "boost::program_options::arg";
+	const auto &symbols = parse_lib("/usr/lib/x86_64-linux-gnu/libc.a", false);
+	const auto symbol_to_find = "__pthread_create";
 	for (const auto &symbol : symbols) {
-		if (symbol.demangled_name() == symbol_to_find) {
+		if (symbol.name == symbol_to_find) {
 			return;
 		}
 	}
 	assume(false);
 }
 
+static void test_reverse_prefix_tree() {
+	Reverse_prefix_tree::Writer tree;
+	std::array words = {"test", "tester", "tree", "tes"};
+	std::array<std::size_t, std::size(words)> indexes;
+	for (std::size_t i = 0; i < std::size(words); ++i) {
+		indexes[i] = tree.add(words[i]);
+		for (std::size_t j = 0; j < i; j++) {
+			assume_equal(tree.test_read(indexes[j]), words[j]);
+			assume_equal(indexes[j], tree.add(words[j]));
+		}
+	}
+
+	std::filesystem::path path = "/tmp/reverse_prefix_tree";
+	tree.write(path);
+
+	Reverse_prefix_tree::Reader reader{path};
+	for (std::size_t i = 0; i < std::size(words); ++i) {
+		assume_equal(reader.get(indexes[i]), words[i]);
+	}
+}
+
 bool test() {
-	for (auto &function : {test_reading_arguments, test_symbol_loading, test_radix_tree}) {
+	for (auto &function : {test_reading_arguments, test_symbol_loading, test_reverse_prefix_tree}) {
 		function();
 	}
 	return true;
